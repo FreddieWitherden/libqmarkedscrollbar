662d33aa9e47db69f03fa9f5039878b337a62fdc
Freddie Witherden <freddie@witherden.org>
2009-04-16 18:33:50 +0100

    Fix the license in the markedscrollbar.[ch].
    Marked Scrollbar => Marked Scroll Bar
    Foobar => Marked Scroll Bar

--- src/markedscrollbar.h
+++ src/markedscrollbar.h
@@ -1,19 +1,19 @@
 /*
-    This file is part of Marked Scrollbar
+    This file is part of Marked Scroll Bar
     Copyright (C) 2009  Freddie Witherden
 
-    Marked Scrollbar is free software: you can redistribute it and/or modify
+    Marked Scroll Bar is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    Marked Scroll Bar is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with Marked Scroll Bar.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
